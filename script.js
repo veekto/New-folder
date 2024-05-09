b0729a2ed385dabf0e9816dab58f42ac1e2a3afb
veekto <105433445+veekto@users.noreply.github.com>
2024-05-09 14:57:38 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+        function veealert(data) {
+    // Alert the provided parameters
+    alert(`title is: ${data.title}, text is: ${data.text}, icon is: ${data.icon}, buttonText is: ${data.buttonText}`);
+    
+    // Your modal logic here to create and display the modal using the provided parameters
+}
